6137cf5973c4899d058597ea08d1e734ba1955ea
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-05-13 21:38:45 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,14 +3,15 @@ echo
 echo
 echo "Installing docker..."
 
-sudo apt-get update -y
+sudo apt-get update -y > /dev/null 2>&1
+sudo yum update -y > /dev/null 2>&1
 curl -fsSL https://get.docker.com -o get-docker.sh > /dev/null 2>&1
 sudo sh ./get-docker.sh  > /dev/null 2>&1
 sudo gpasswd -a ubuntu docker > /dev/null 2>&1
 sudo gpasswd -a jenkins docker > /dev/null 2>&1
 echo "Docker installed sucessfully"
 newgrp docker
-sudo systemctl restart jenkins
+sudo systemctl restart jenkins > /dev/null 2>&1
 docker search ubuntu
 if [ $? -ne 0 ]; then
   echo "If you're seeing permission denied error, please close the terminal and reopen it to get the updated docker group permission"

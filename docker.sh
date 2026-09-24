ec3b8c43554b76beffdc9ef95a88f67bee1c9bd1
nagaraj602 <264412729+nagaraj602@users.noreply.github.com>
2026-09-24 15:36:49 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -4,6 +4,7 @@ echo
 echo "Installing docker..."
 
 sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get upgrade -y  > /dev/null 2>&1
 sudo yum update -y > /dev/null 2>&1
 sudo apt-get install login -y > /dev/null 2>&1
 sudo dnf install shadow-utils -y > /dev/null 2>&1
